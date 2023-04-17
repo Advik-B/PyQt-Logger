d01df29098dfe07804dde19272f024b176fdf163
Advik <advik.b@gmail.com>
2023-04-17 21:24:55 +0530

    Update logger_started.py

--- QtLogger/execptions/logger_started.py
+++ QtLogger/execptions/logger_started.py
@@ -1,1 +1,5 @@
-class LoggerNotStartedException(Exception): pass
+class LoggerNotStartedException(Exception):
+    """
+    This exception is raised when you try to log something without starting the logger
+    """
+    
