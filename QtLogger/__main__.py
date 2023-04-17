4a821b2c3d7dc3aaced6d8fdb735ecc6b945b90f
Advik <advik.b@gmail.com>
2023-04-17 20:36:33 +0530

    Update __main__.py

--- QtLogger/__main__.py
+++ QtLogger/__main__.py
@@ -28,16 +28,28 @@ class QtLogger(QWidget):
         # Highlight things like these
         # [DEBUG]-[time]-[module]: message <--- This one is purple
         # [INFO]-[time]-[module]: message <--- This one is blue
-        # [WARNING]-[t  ime]-[module]: message <--- This one is yellow
+        # [WARNING]-[time]-[module]: message <--- This one is yellow
         # [ERROR]-[time]-[module]: message <--- This one is red
         # [SUCCESS]-[time]-[module]: message <--- This one is green
-
         # Set the font
         self.logger_view.setFont(self.font)
         # Date
         self.date = datetime.now().strftime("%d-%m-%Y")
 
+    def start(self):
+
+
     def log(self, message: str, level: str = "INFO"):
         # Get the module name from the stack and store it in a variable
         module_name = inspect.stack()[1][1].split("\\")[-1].split(".")[0]
         time = datetime.now().strftime("%H:%M:%S")
+        # If the level is not in the LOG_LEVELS dict, set it to INFO
+        if level not in LOG_LEVELS:
+            level = "INFO"
+        # If the level is in the custom_colors dict, use that color
+        colour = self.custom_colors[level] # We don't need to check if it's in the dict because we already checked that in the if statement above
+        # Create the log message
+        log_message = f"[{level}]-[{time}]-[{module_name}]: {message}"
+        # Add the log message to the logger view with the correct color
+        self.logger_view.append(f"<font color={colour}>{log_message}</font>")
+
