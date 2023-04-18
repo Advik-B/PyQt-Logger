9d8c32e6122923dfd51077cb72a3420a316c0dd9
Advik <advik.b@gmail.com>
2023-04-18 11:44:04 +0530

    Update __main__.py

--- QtLogger/__main__.py
+++ QtLogger/__main__.py
@@ -1,15 +1,17 @@
 from .base import QtLogger
 from PyQt6.QtGui import QFont
-from PyQt6.QtWidgets import QWidget, QApplication
-
+from PyQt6.QtWidgets import QWidget, QApplication, QGridLayout
 from sys import argv
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.logger = QtLogger(self, log_folder="logs", font=QFont("JetBrains Mono", 10))
+        self.lay = QGridLayout(self)
+        self.logger = QtLogger(self, log_folder="logs", font=QFont("Consolas", 10))
         self.logger.start()
+        self.lay.addWidget(self.logger)
+
         self.show()
 
     def closeEvent(self, event):
